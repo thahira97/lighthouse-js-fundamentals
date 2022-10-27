ae7d41553fbc496ea80395a15298bea6090a9eaa
thahira97 <thahirasheerin3@gmail.com>
2022-10-26 21:19:01 -0400

    Add Conditionals and Tinkering

---
+++ tinkering.js
@@ -0,0 +1,89 @@
+function multiply (a,b) {
+  return a*b
+}
+const result =multiply (2,4);
+console.log(result);
+
+let apple =[1,4,6]
+console.log(apple.length*6);
+
+let number1= 46787;
+let number2= 746587;
+let number3=number1+number2;
+console.log(number3);
+
+let apples=60;
+
+var oranges=987;
+if(apples<oranges){
+  console.log(97+987)
+};
+let bers=2+10-19+4-90+1
+console.log(bers);
+const arith= -20+-19-(-10)-(-1)+24;
+console.log(arith);
+let sums = (10/5)*4-20;
+console.log(sums);
+
+const divide = 4096%12;
+console.log(divide)
+
+let a=43;
+let b=47
+console.log(a>b);
+
+let x=12;
+let y=17;
+console.log(12==17);
+
+const s=3;
+let u=3;
+console.log(3<=3);
+let r=1;
+var t=0
+console.log(1!=0);
+
+let xy= 1+1+2+3+5;
+var yz='';
+  console.log(20-xy);
+
+console.log(25+37)
+console.log(62/12)
+let word="Hello+5*10";
+console.log(word)
+
+var my_string = "T";
+var ASCII_value = my_string.charCodeAt(0);
+console.log(ASCII_value);
+
+var my_string = "Udacity";
+
+// Iterate using a Loop
+for (var i = 0; i < my_string.length; i++) {
+  console.log(my_string.charCodeAt(i));
+}
+if ("green">"blue"){
+  console.log(true)
+}
+else {console.log(false)};
+
+console.log("3">1);
+console.log(3!="3");
+console.log(true>=0);
+console.log(1!==false);
+console.log("false"===0);
+console.log("Hello World!");
+let ten=10;
+console.log(ten + 3)
+console.log(ten*3)
+let bill=20;
+let tip=15%20;
+console.log(tip+bill);
+console.log(4+'6');
+
+if ((3!=6%3)&&!(24>45)&&(!false)){
+  console.log(true)
+}
+else {
+  console.log(false)
+};
